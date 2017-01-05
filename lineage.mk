722ad6451e508c33f462f6ec573f8dc796b1b2c8
Oliver Scott <olivercscott@gmail.com>
2017-01-05 14:58:38 -0500

    jflteatt: Rebrand to LineageOS and Remove Enhanced NFC
    Enhanced NFC is obsolete
    
    Change-Id: Icfd66d493f157e3c14c5ea5b91ba41fd0e34e05e

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -12,5 +9,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:5.0.1/LRX22C/I337UCUGOC3:user/release-keys" \
   PRIVATE_BUILD_DESC="jflteuc-user 5.0.1 LRX22C I337UCUGOC3 release-keys"
 
-PRODUCT_NAME := cm_jflteatt
+PRODUCT_NAME := lineage_jflteatt
 PRODUCT_DEVICE := jflteatt
