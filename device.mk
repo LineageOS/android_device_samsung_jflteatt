LineageOS/android_device_samsung_jfltexx

jfltexx: Call device's system prop using makefile
* Also declare the vendor security patch

Change-Id: Ib59f8bae67c94dcfc4674049bc5f905ef40df3c4
Co-Authored-By: dkati <dkatikaridis@gmail.com>

device.mk
@@ -22,3 +22,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltexx/overlay
 
 # Inherit from jf-common
 $(call inherit-product, device/samsung/jf-common/jf-common.mk)
+
+# Call jfltexx system props
+$(call inherit-product, device/samsung/jfltexx/system_prop.mk)

LineageOS/android_device_samsung_jflteatt

device.mk
@@ -31,3 +31,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/jflteatt/overlay
 
 # Inherit from jf-common
 $(call inherit-product, device/samsung/jf-common/jf-common.mk)
+
+# Call jflteatt system props
+$(call inherit-product, device/samsung/jflteatt/system_prop.mk)