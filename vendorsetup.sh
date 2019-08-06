LineageOS/android_device_samsung_jfltexx

jfltexx: Push the {setup-makefiles|vendorsetup} script
Change-Id: I503d8d716c6a56509afc01c2233d1fa805540eb9
Co-Authored-By: dkati <dkatikaridis@gmail.com>

vendorsetup.sh
@@ -1,0 +1,2 @@
+add_lunch_combo lineage_jfltexx-eng
+add_lunch_combo lineage_jfltexx-userdebug

LineageOS/android_device_samsung_jflteatt

vendorsetup.sh
@@ -1,0 +1,2 @@
+add_lunch_combo lineage_jflteatt-eng
+add_lunch_combo lineage_jflteatt-userdebug