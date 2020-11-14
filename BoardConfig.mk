74367488c600bbf191125a56f3533e064ceee83b
Arne Coucheron <arco68@gmail.com>
2020-11-14 10:01:17 +0100

    Revert "jflteatt: Create manifest"
    Moved to fragment in jf-common.
    
    This reverts commit 688e9280dbe4b66607324981f9c337a05abed98f.
    
    Change-Id: Icdb6e131cf8c8e69a5792b60f601c1e0acfca1fc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,6 @@
 # Inherit from proprietary vendor
 -include vendor/samsung/jflteatt/BoardConfigVendor.mk
 
-# Manifest
-DEVICE_MANIFEST_FILE += device/samsung/jflteatt/manifest.xml
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := jflteatt
 
